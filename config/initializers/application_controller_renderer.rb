5f36436dadf5644e31e9ef8063c6c2ac1e4768f1
Ami-tnk <ami1y251992@gmail.com>
2020-11-28 06:28:48 +0000

    [Add]投稿者画像

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'eae30e12289b04328229f221e3ddeba6aaeaefdd1a032f2d9808e0cd97bf9d1a29fa92d04ddfa930ec0df5ce6717bfe38a95263d0e716017d36c01a3b28c7aef'
+
