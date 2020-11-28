de3e935c87031e029fda171798c95a5905a529c4
Ami-tnk <ami1y251992@gmail.com>
2020-11-28 08:25:48 +0000

    [Update]index,show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.new
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page]).reverse_order
