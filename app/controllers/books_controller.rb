d5cac72300635b62f7649c2c81e33e6261195158
Ami-tnk <ami1y251992@gmail.com>
2020-11-29 09:39:07 +0000

    [Add]flashメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -39,7 +39,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
 
     if @book.update(book_params)
-      redirect_to book_path(@book.id),notice:"Successfully"
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
       render 'edit'
     end
@@ -48,7 +48,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path, notice:"successfully"
+    redirect_to books_path
   end
 
   private
